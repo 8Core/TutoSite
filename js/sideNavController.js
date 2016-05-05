5f3d73e44173ff571c5cba763df602b5fe8c1a80
esvin <esvin.jose.estrada@gmail.com>
2016-05-05 00:04:35 -0600

    First Window

--- js/sideNavController.js
+++ js/sideNavController.js
@@ -6,12 +6,13 @@ angular
 
   function sideNavCtrl($scope,$mdSidenav)
   {
+      var vm = this;
     $scope.toggleSidenav = function(menuId) {
       $mdSidenav(menuId).toggle();
     };
 
     var imagePath = 'img/list/60.jpeg';
-    $scope.todos = [
+    vm.todos = [
       {
         face : 'action:ic_dashboard_24px',
         what: 'Brunch this weekend?',
@@ -25,6 +26,36 @@ angular
         who: 'Min Li Chan',
         when: '3:08PM',
         notes: " I'll be in your neighborhood doing errands"
+      }
+    ];
+    /* Items del Menu */
+    vm.menu = [
+      {
+        link : '',
+        title: 'Dashboard',
+        icon: 'action:ic_dashboard_24px' // we have to use Google's naming convention for the IDs of the SVGs in the spritesheet
+      },
+      {
+        link : '',
+        title: 'Friends',
+        icon: 'social:ic_group_24px'
+      },
+      {
+        link : '',
+        title: 'Messages',
+        icon: 'communication:ic_message_24px'
+      }
+    ];
+    vm.admin = [
+      {
+        link : '',
+        title: 'Trash',
+        icon: 'action:ic_delete_24px'
+      },
+      {
+        link : 'showListBottomSheet($event)',
+        title: 'Settings',
+        icon: 'action:ic_settings_24px'
       }
     ];
   }
